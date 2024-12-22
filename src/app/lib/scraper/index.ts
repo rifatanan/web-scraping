72e46f755bcf87b37678b7307de7d8549d5352b8
rifatanan <rifatanan017@gamil.com>
2024-12-22 23:37:23 +0600

    add utils

--- src/app/lib/scraper/index.ts
+++ src/app/lib/scraper/index.ts
@@ -1,20 +1,21 @@
+'use server';
+
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { extractPrice } from '../utils';
+import { extractCurrency, extractDescription, extractPrice } from '../utils';
 
-export async function scrapedAmazonProduct(url: string) {
-    console.log('scrapper index');
-
+export async function scrapeAmazonProduct(url: string) {
     if (!url) return;
 
-    const userName = String(process.env.BRIGHT_DATA_USERNAME);
+    // BrightData proxy configuration
+    const username = String(process.env.BRIGHT_DATA_USERNAME);
     const password = String(process.env.BRIGHT_DATA_PASSWORD);
-    const port = 33335;
-    const session_id = (100000 * Math.random()) | 0;
+    const port = 22225;
+    const session_id = (1000000 * Math.random()) | 0;
 
     const options = {
         auth: {
-            username: `${userName}-session-${session_id}`,
+            username: `${username}-session-${session_id}`,
             password,
         },
         host: 'brd.superproxy.io',
@@ -23,55 +24,67 @@ export async function scrapedAmazonProduct(url: string) {
     };
 
     try {
+        // Fetch the product page
         const response = await axios.get(url, options);
         const $ = cheerio.load(response.data);
+
+        // Extract the product title
         const title = $('#productTitle').text().trim();
         const currentPrice = extractPrice(
-            $(
-                'span.a-price.a-text-price.a-size-medium.apexPriceToPay.a-price-whole',
-            ),
-            $('span.a-offscreen'),
+            $('.priceToPay span.a-price-whole'),
+            $('.a.size.base.a-color-price'),
+            $('.a-button-selected .a-color-base'),
         );
 
-        const orginalPrice = extractPrice(
-            $(
-                'span.a-price.a-text-price.a-size-medium.apexPriceToPay.a-price-whole',
-            ),
-            $('span.a-offscreen'),
+        const originalPrice = extractPrice(
+            $('#priceblock_ourprice'),
+            $('.a-price.a-text-price span.a-offscreen'),
+            $('#listPrice'),
+            $('#priceblock_dealprice'),
+            $('.a-size-base.a-color-price'),
         );
 
-        const availableStock = $('#availability .a-size-medium.a-color-success')
+        const outOfStock =
+            $('#availability span').text().trim().toLowerCase() ===
+            'currently unavailable';
+
+        const images =
+            $('#imgBlkFront').attr('data-a-dynamic-image') ||
+            $('#landingImage').attr('data-a-dynamic-image') ||
+            '{}';
+
+        const imageUrls = Object.keys(JSON.parse(images));
+
+        const currency = extractCurrency($('.a-price-symbol'));
+        const discountRate = $('.savingsPercentage')
             .text()
-            .trim();
+            .replace(/[-%]/g, '');
 
-        const images = $('#landingImage').attr('data-a-dynamic-image') || '{}';
-        const imagesUrls = Object.keys(JSON.parse(images));
+        const description = extractDescription($);
 
-        const currency = orginalPrice.slice(0, 1);
-
-        console.log(
-            'title:',
-            title +
-                '\ncurrentPrice:' +
-                currentPrice +
-                '\noriginalPrice:' +
-                orginalPrice,
-            '\navailableStock:' + availableStock,
-            '\nImage:' + imagesUrls,
-            '\nCurrency:' + currency,
-        );
-
+        // Construct data object with scraped information
         const data = {
             url,
-            currency: currency || '0$',
-            image: images[0],
+            currency: currency || '$',
+            image: imageUrls[0],
             title,
-            currentPrice,
-            orginalPrice,
+            currentPrice: Number(currentPrice) || Number(originalPrice),
+            originalPrice: Number(originalPrice) || Number(currentPrice),
+            priceHistory: [],
+            discountRate: Number(discountRate),
+            category: 'category',
+            reviewsCount: 100,
+            stars: 4.5,
+            isOutOfStock: outOfStock,
+            description,
+            lowestPrice: Number(currentPrice) || Number(originalPrice),
+            highestPrice: Number(originalPrice) || Number(currentPrice),
+            averagePrice: Number(currentPrice) || Number(originalPrice),
         };
 
         console.log(data);
-    } catch (error) {
-        console.error('Error fetching product page:', error);
+        return data;
+    } catch (error: any) {
+        console.log(error);
     }
 }
